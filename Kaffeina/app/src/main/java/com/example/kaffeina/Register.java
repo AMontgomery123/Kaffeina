9e68093d8bfe9b55a367e6a85f2b7013657371fb
Scott LoCascio <scottlocascio@gmail.com>
2020-10-18 13:08:00 -0700

    got registration to a testable state. Can register user accounts but there is an error when signing up even though the account is successfully created.  will debug in later commit

--- Kaffeina/app/src/main/java/com/example/kaffeina/Register.java
+++ Kaffeina/app/src/main/java/com/example/kaffeina/Register.java
@@ -1,20 +1,28 @@
 package com.example.kaffeina;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.widget.TextView;
 import android.widget.EditText;
 import android.widget.Button;
 import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
-   TextView nameField;
-   EditText emailField, passwordField;
-   FirebaseAuth Gandalph;
-   Button registerButton;
+    TextView nameField;
+    EditText emailField, passwordField;
+    FirebaseAuth Gandalph;
+    Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,15 +35,42 @@ public class Register extends AppCompatActivity {
         nameField = findViewById(R.id.fullName);
         registerButton = findViewById(R.id.registerButton);
 
-        registerButton.setOnClickListener(new View.OnClickListener(){
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 String email = emailField.getText().toString();
                 String password = passwordField.getText().toString();
                 String name = nameField.getText().toString();
+                boolean missingFields = false;
 
-                if(email.isEmpty() || password.isEmpty()){
-                    //tell user to go fuck themselves
+                if (email.isEmpty()) {
+                    emailField.setError("Please enter an email address");
+                    emailField.requestFocus();
+                    missingFields = true;
+                } else if (password.isEmpty()) {
+                    passwordField.setError("Please enter a password");
+                    passwordField.requestFocus();
+                    missingFields = true;
+                } else if (name.isEmpty()) {
+                    nameField.setError("Please enter a name");
+                    nameField.requestFocus();
+                    missingFields = true;
+                }
+
+                if (missingFields) {
+                    Toast.makeText(Register.this, "Please fill in all fields and re-submit", Toast.LENGTH_SHORT).show();
+                } else {
+                    Gandalph.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(Register.this, "SignUp unsuccessful.  Did you sell your soul to Milhouse again?", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                startActivity(new Intent(Register.this, MainActivity.class));
+                            }
+                        }
+                    });
                 }
             }
         });
