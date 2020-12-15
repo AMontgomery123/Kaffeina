a01af9aac2be20358ae776c143c8c1b6f210ce1c
DESKTOP-9B068CL\Exodus <alex.t.montgomery@gmail.com>
2020-12-14 23:59:15 -0800

    Updated manifest and create restaurant

--- Kaffeina/app/src/main/java/com/example/kaffeina/CreateRestaurant.java
+++ Kaffeina/app/src/main/java/com/example/kaffeina/CreateRestaurant.java
@@ -36,7 +36,7 @@ public class CreateRestaurant extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_review);
+        setContentView(R.layout.activity_restaurant);
 
         Intent intent = getIntent();
         Restaurant current_restaurant = (Restaurant) intent.getSerializableExtra("Restaurant ID");
