111d71293a19b16a2d4df283c577496e0af1ca7b
AddisonFung <42878755+AddisonFung@users.noreply.github.com>
2020-12-15 23:34:37 -0800

    Update User.java

--- Kaffeina/app/src/main/java/com/example/kaffeina/User.java
+++ Kaffeina/app/src/main/java/com/example/kaffeina/User.java
@@ -4,6 +4,7 @@ import com.google.firebase.database.IgnoreExtraProperties;
 
 @IgnoreExtraProperties
 public class User {
+    //user has an id, username, email, and a count of how many reviews they have done
     public String uid;
     public String userName;
     public String userEmail;
@@ -13,15 +14,18 @@ public class User {
     }
 
     public User(String uid, String userName, String userEmail, int reviewCount){
+        //constructur
         this.uid = uid;
         this.userName = userName;
         this.userEmail = userEmail;
         this.reviewCount = reviewCount;
     }
     public void increment(){
+        //increment the amount of reviews by 1
         this.reviewCount++;
     }
     public void setUser(User newUser) {
+        //set the user as a new user
         this.uid = newUser.uid;
         this.userName = newUser.userName;
         this.userEmail = newUser.userEmail;
@@ -29,6 +33,7 @@ public class User {
     }
 
     public String getString(){
+        //return the user's information as a string
         String returnString = this.uid + " " + this.userName + " " + this.userEmail + " " + Integer.toString(this.reviewCount);
         return returnString;
     }
